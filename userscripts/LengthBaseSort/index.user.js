c2772ce4308ba9715e5b45451c332580c9bbb5c2
Mikarific <mikerific2@gmail.com>
2024-10-24 14:11:47 -0600

    LengthBaseSort Patch

--- userscripts/LengthBaseSort/index.user.js
+++ userscripts/LengthBaseSort/index.user.js
@@ -3,8 +3,8 @@
 // @namespace   Violentmonkey Scripts4
 // @match       https://neal.fun/infinite-craft/*
 // @grant       unsafeWindow
-// @version     1.0
-// @author      Alexander_Andercou
+// @version     1.1
+// @author      Alexander_Andercou, Mikarific
 // @description 5/23/2024, 7:54:23 PM
 // ==/UserScript==
 
@@ -12,12 +12,12 @@
   window.addEventListener('load', async () => {
     console.log("Welcome to hijack sorting length");
     let new_svg = 'data:image/svg+xml;base64,PHN2ZyB2aWV3Qm94PSIwIDAgNjQgNjQiIHhtbG5zPSJodHRwOi8vd3d3LnczLm9yZy8yMDAwL3N2ZyI+PHBhdGggZD0iTS43IDYxLjhDLS41IDYzLS4xIDY0IDEuNiA2NGg1OS4zYzEuNyAwIDMuMS0xLjQgMy4xLTMuMVYxLjZjMC0xLjctMS0yLjEtMi4yLS45em00OS4xLTE1YzAgMS43LTEuNCAzLjEtMy4xIDMuMWgtMTFjLTEuNyAwLTIuMS0xLS45LTIuMmwxMi44LTEyLjhjMS4yLTEuMiAyLjItLjggMi4yLjl6IiBmaWxsPSIjZmZjZTMxIi8+PHBhdGggZD0iTTIuNCA2MS4zaDFWNjRoLTF6bTIuOSAwaDFWNjRoLTF6bTMgMGgxVjY0aC0xem0zLTIuM2gxdjVoLTF6bTMuMSAyLjNoMVY2NGgtMXptMyAwaDFWNjRoLTF6bTMgMGgxVjY0aC0xem0zLTIuM2gxdjVoLTF6bTMuMSAyLjNoMVY2NGgtMXptMyAwaDFWNjRoLTF6bTMgMGgxVjY0aC0xem0yLjktMi4zaDF2NWgtMXptMy4yIDIuM2gxVjY0aC0xem0yLjkgMGgxVjY0aC0xem0zIDBoMVY2NGgtMXptMy0yLjNoMXY1aC0xem0zLjEgMi4zaDFWNjRoLTF6bTMgMGgxVjY0aC0xem0zIDBoMVY2NGgtMXptMy0yLjNoMXY1aC0xeiIgZmlsbD0iIzg5NjY0YyIvPjwvc3ZnPg==';
-    unsafeWindow.$nuxt.$root.$children[2].$children[0].$children[0].sorts.push("length");
+    unsafeWindow.$nuxt.$root.$children[1].$children[0].$children[0].sorts.push("length");
     let elementals = null;
-    const complex_filter = unsafeWindow.$nuxt.$root.$children[2].$children[0].$children[0]._computedWatchers.sortedElements.getter;
-    unsafeWindow.$nuxt.$root.$children[2].$children[0].$children[0]._computedWatchers.sortedElements.getter =
+    const complex_filter = unsafeWindow.$nuxt.$root.$children[1].$children[0].$children[0]._computedWatchers.sortedElements.getter;
+    unsafeWindow.$nuxt.$root.$children[1].$children[0].$children[0]._computedWatchers.sortedElements.getter =
       exportFunction(() => {
-        if (unsafeWindow.$nuxt.$root.$children[2].$children[0].$children[0]._data.sortBy === "length") {
+        if (unsafeWindow.$nuxt.$root.$children[1].$children[0].$children[0]._data.sortBy === "length") {
           let img = document.querySelector(".sidebar-sort > img");
           img.style.filter = "none";
           img.style.filter = "invert(0)";
@@ -26,14 +26,14 @@
           return elements2.sort((a, b) => a.text.length.toString().localeCompare(b.text.length.toString(), undefined, { 'numeric': true }));
         } else {
           let elements2 = [...complex_filter.call(this)];
-          console.log("By:", unsafeWindow.$nuxt.$root.$children[2].$children[0].$children[0]._data.sortBy);
+          console.log("By:", unsafeWindow.$nuxt.$root.$children[1].$children[0].$children[0]._data.sortBy);
           return elements2;
         }
       }, unsafeWindow);
     const sortButtonObserver = new MutationObserver((mutations) => {
-      if (unsafeWindow.$nuxt.$root.$children[2].$children[0].$children[0]._data.sortBy === "length") {
+      if (unsafeWindow.$nuxt.$root.$children[1].$children[0].$children[0]._data.sortBy === "length") {
         let img = document.querySelector(".sidebar-sort > img");
-        console.log("mutation:", unsafeWindow.$nuxt.$root.$children[2].$children[0].$children[0]._data.sortBy);
+        console.log("mutation:", unsafeWindow.$nuxt.$root.$children[1].$children[0].$children[0]._data.sortBy);
         if (img.src.trim() == "https://neal.fun/infinite-craft/length.svg") {
           img.src = new_svg;
           img.style.filter = "invert(0)";

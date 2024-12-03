7077f6fd498683288ea9f869bd55bab120ae17f7
Margen67 <Margen67@users.noreply.github.com>
2024-12-02 23:29:42 -0800

    Use __vue__ instead of unsafeWindow spaghetti

--- userscripts/QuickDelete/index.user.js
+++ userscripts/QuickDelete/index.user.js
@@ -2,37 +2,38 @@
 // @name        Quick Delete
 // @namespace   Quick Delete
 // @match       https://neal.fun/infinite-craft/*
-// @grant       unsafeWindow
 // @version     1.1
 // @author      Margen67, Mikarific
 // @description Quickly delete elements by holding CTRL+RMB and hovering over them.
 // ==/UserScript==
 
-window.addEventListener("load", () => {
-  function setRightClickOnMutations(mutations) {
-    for (const mutation of mutations) {
-      if (mutation.addedNodes.length > 0) {
-        for (const node of mutation.addedNodes) {
-          node.addEventListener('mouseover', (e) => {
-            if (e.ctrlKey && e.buttons === 2) {
-              function isMatch(instance) {
-                if (instance.elem) {
-                  return instance.elem.id !== e.target.id;
-                } else {
-                  return false;
+(function() {
+  window.addEventListener("load", () => {
+    function setRightClickOnMutations(mutations) {
+      for (const mutation of mutations) {
+        if (mutation.addedNodes.length > 0) {
+          for (const node of mutation.addedNodes) {
+            node.addEventListener('mouseover', (e) => {
+              if (e.ctrlKey && e.buttons === 2) {
+                function isMatch(instance) {
+                  if (instance.elem) {
+                    return instance.elem.id !== e.target.id;
+                  } else {
+                    return false;
+                  };
                 };
+                const vue = document.querySelector(".container").__vue__;
+                vue._data.instances = vue._data.instances.filter(isMatch);
               };
-              unsafeWindow.$nuxt.$root.$children[1].$children[0].$children[0]._data.instances =
-                unsafeWindow.$nuxt.$root.$children[1].$children[0].$children[0]._data.instances.filter(isMatch);
-            };
-          });
+            });
+          };
         };
       };
     };
-  };
-  const instanceObserver = new MutationObserver((mutations) => {
-    setRightClickOnMutations(mutations);
+    const instanceObserver = new MutationObserver((mutations) => {
+      setRightClickOnMutations(mutations);
+    });
+    const instances = document.querySelector('.instances');
+    instanceObserver.observe(instances, { childList: true, subtree: true });
   });
-  const instances = document.querySelector('.instances');
-  instanceObserver.observe(instances, { childList: true, subtree: true });
-});
+})();

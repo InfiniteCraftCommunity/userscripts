695e6d26742d0c5b6c3ec3452ac4e53a8f38afd8
Roman A <121314722+GameRoMan@users.noreply.github.com>
2025-07-15 18:48:50 +0100

    Fix color-proven userscript performance

--- userscripts/gameroman/color-proven/index.user.js
+++ userscripts/gameroman/color-proven/index.user.js
@@ -7,106 +7,76 @@
 // @downloadURL     https://userscripts.rman.dev/infinite-craft/color-proven/index.user.js
 // @updateURL       https://userscripts.rman.dev/infinite-craft/color-proven/index.user.js
 // @name            Color proven Elements
-// @match           https://neal.fun/infinite-craft
-// @version         1.3.3
+// @match           https://neal.fun/infinite-craft/
+// @version         2.0
 // @description     Colors elements that are proven to Base Eements in green color
 // ==/UserScript==
 
-(function () {
-  let mapElements = {};
+(function() {
+  const green_color = "#00cc1f";
+  const red_color = "#ff1c1c";
+  let elementsMap = {};
   async function load_data(location) {
-    const response = await fetch(
-      `https://glcdn.githack.com/gameroman/infinite-craft/-/raw/main/base-elements/${location}.json`,
-      { cache: "no-store" }
-    );
+    const response = await fetch(`https://glcdn.githack.com/gameroman/infinite-craft/-/raw/main/base-elements/${location}.json`, { cache: "no-store" });
     return await response.json();
   }
-  const __VUE__ = document.querySelector(".container").__vue__;
   async function colorElements(green, red) {
-    const green_color = "#00cc1f";
-    const red_color = "#ff1c1c";
-    const interval = setInterval(() => {
-      if (
-        document.querySelector(".container").__vue__._data.elements.length > 0
-      ) {
-        clearInterval(interval);
-        mapElements = {};
-        let items = Array.from(document.querySelectorAll(".item"));
-        for (let elem of green) {
-          mapElements[elem.toLowerCase()] = { color: green_color };
-          let elemNode = __VUE__._data.elements.find(
-            (x) => x.text.toLowerCase() == elem.toLowerCase()
-          );
-          if (elemNode) {
-            elemNode.color = green_color;
-            let instancesGreen = __VUE__._data.instances.filter(
-              (x) => x.text.toLowerCase() == elem.toLowerCase()
-            );
-            instancesGreen.forEach((x) => {
-              if (x.elem) x.elem.style.color = elemNode.color;
-            });
-            let itemsGreen = items.filter(
-              (x) =>
-                x.childNodes[1].data.trim().toLowerCase() ==
-                elem.trim().toLowerCase()
-            );
-            itemsGreen.forEach((x) => (x.style.color = elemNode.color));
-          }
-        }
-        for (let elem of red) {
-          mapElements[elem.toLowerCase()] = { color: red_color };
-          let elemNode = __VUE__._data.elements.find((x) => x.text == elem);
-          if (elemNode) {
-            elemNode.color = red_color;
-            let instancesRed = __VUE__._data.instances.filter(
-              (x) => x.text.toLowerCase() == elem.toLowerCase()
-            );
-            instancesRed.forEach((x) => {
-              if (x.elem) x.elem.style.color = elemNode.color;
-            });
-            let itemsRed = items.filter(
-              (x) =>
-                x.childNodes[1].data.trim().toLowerCase() ==
-                elem.trim().toLowerCase()
-            );
-            itemsRed.forEach((x) => (x.style.color = elemNode.color));
-          }
-        }
-      }
-    }, 300);
+    elementsMap = {};
+    for (const elem of green) {
+      elementsMap[elem.toLowerCase()] = { color: green_color };
+    }
+    for (const elem of red) {
+      elementsMap[elem.toLowerCase()] = { color: red_color };
+    }
   }
-  window.addEventListener("helper-load", async () => {
+  function handleNode(node) {
+    if (!(node instanceof HTMLElement))
+      return;
+    console.log(node, 0);
+    if (node.classList.contains("instance") && node.querySelector(".instance-emoji")) {
+      const this_node = node;
+      const instance = IC.getInstances().find((x) => x.element == this_node);
+      if (!instance)
+        return;
+      const elem = elementsMap[instance.text.toLowerCase()];
+      if (elem && elem.color) {
+        this_node.style.color = elem.color;
+      }
+    } else if (node.classList.contains("item-wrapper") && node.querySelector(".item")) {
+      const this_node = node;
+      const item = Array.from(this_node.querySelectorAll(".item")).find((x) => x == this_node.children[0]);
+      if (!item)
+        return;
+      const text = item.childNodes[1].textContent;
+      if (!text)
+        throw new Error("Something went wrong!");
+      const elem = elementsMap[text.trim().toLowerCase()];
+      if (elem && elem.color) {
+        this_node.style.color = elem.color;
+      }
+    }
+  }
+  window.addEventListener("load", async () => {
     const proven = await load_data("proven");
     const disproven = await load_data("disproven");
+    const interval = setInterval(() => {
+      if (!IC)
+        return;
+      clearInterval(interval);
+    }, 3000);
     await colorElements(proven, disproven);
     const instanceObserver = new MutationObserver((mutations) => {
       for (const mutation of mutations) {
-        if (mutation.addedNodes.length > 0) {
-          for (const node of mutation.addedNodes) {
-            if (
-              node.id != "instance-0" &&
-              node.classList.contains("instance") &&
-              node.querySelector(".instance-emoji")
-            ) {
-              let instance = __VUE__._data.instances.find(
-                (x) => x.elem == node
-              );
-              if (instance) {
-                let elem = mapElements[instance.text.toLowerCase()];
-                if (elem && elem.color) {
-                  node.style.color = elem.color;
-                }
-              }
-            } else {
-            }
-          }
-        } else {
+        if (!mutation.addedNodes.length)
+          return;
+        for (const node of mutation.addedNodes) {
+          handleNode(node);
         }
       }
     });
-    instanceObserver.observe(document.getElementsByClassName("instances")[0], {
+    instanceObserver.observe(document.querySelector(".infinite-craft"), {
       childList: true,
-      subtree: true,
+      subtree: true
     });
   });
 })();

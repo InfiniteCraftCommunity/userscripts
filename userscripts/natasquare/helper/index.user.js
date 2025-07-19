a032aa528643e2b0fc14cdf463e44242269458dc
ntsq <Natasquare@users.noreply.github.com>
2025-07-19 16:20:11 +0700

    chore(helper): bump version

--- userscripts/natasquare/helper/index.user.js
+++ userscripts/natasquare/helper/index.user.js
@@ -5,7 +5,7 @@
 // @grant		GM.addStyle
 // @grant		unsafeWindow
 // @run-at		document-start
-// @version		1.1.2
+// @version		1.2.0
 // @author		Natasquare
 // @description	Adds various QoL features to Infinite Craft - a port of Mikarific's Infinite Craft Helper.
 // @updateURL	https://raw.githubusercontent.com/InfiniteCraftCommunity/userscripts/master/userscripts/natasquare/helper/index.user.js
